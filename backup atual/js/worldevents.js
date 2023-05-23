e0c50d328c331bf8b2ba219dff15353e7d3224ea
bagrieltinti <133942252+bagrieltinti@users.noreply.github.com>
2023-05-22 21:27:15 -0300

    Add files via upload

---
+++ backup atual/js/worldevents.js
@@ -0,0 +1,22 @@
+// Definição dos eventos de mundo
+const worldEvents = [
+  'Um youtuber viraliza fazendo vídeo de opinião sobre outro youtuber, putz.',
+  'Influenciador é cancelado no twitter.',
+  'Criador de conteúdo viraliza ao usar maquiagem feita por outra influenciadora: "Acho que isso não ficou certo".',
+  'Música top 1 das rádios faz referência a bandas clássicas de rock. 🚶‍♂️🚶🚶🏻‍♀️🚶🏽',
+  'Lançou novo filme dos Vingadores',
+  'Lançou novo filme do Super Mario',
+  'Lançou novo GTA',
+  'Lançou novo filme dos Vingadores',
+];
+
+// Função para obter um evento de mundo aleatório
+function getRandomWorldEvent() {
+  const randomIndex = Math.floor(Math.random() * worldEvents.length);
+  return worldEvents[randomIndex];
+}
+
+// Atribuição direta no objeto global (window)
+window.worldEventsModule = {
+  getRandomWorldEvent
+};
